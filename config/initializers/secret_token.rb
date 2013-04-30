543ddceafdd8517c9326ae239a1c4f8529fac9ad
Jen Lim <jennolim@gmail.com>
2013-04-30 15:33:43 -0400

    Start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bl2::Application.config.secret_token = 'a39093ba2723415611b8ed8deef0d3004069950e0afd3054fe4b291b0cfcf10f117eb216ebea3014fcdf505e77f1db7d40a7304048824569d7517597c0fb8f0c'
